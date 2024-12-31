dbe23304496f602743da7675ef1803818e04a275
radhoda <rod.hoda@gmail.com>
2024-12-31 13:35:01 -0800

    Basic text writing and editing now possible

--- editor/gui.py
+++ editor/gui.py
@@ -9,8 +9,25 @@ class TextEditor(tk.Tk):
         self.geometry("800x600")
 
         self._create_menu_bar()
+        self._create_text_box()
 
     def _create_menu_bar(self):
         menubar = tk.Menu(self)
 
+        file_menu = tk.Menu(menubar, tearoff=0)
+
+        file_menu.add_command(label="New")
+        file_menu.add_command(label="Open")
+        file_menu.add_command(label="Save")
+        file_menu.add_command(label="Save As")
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit")
+
+        menubar.add_cascade(label="File", menu=file_menu)
+
+        self.config(menu=menubar)
+
+    def _create_text_box(self):
+        self.text_area = tk.Text(self, wrap="word", undo=True)
+        self.text_area.pack(expand=True, fill=tk.BOTH)
 
